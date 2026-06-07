79a218336c74bd49752ff33aa28af0d57b7ee754
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 14:45:26 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio3/ejercicio3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N1, N2;
+        Console.Write("Ingrese numero:");
+        N1= Convert.ToInt32(Console.ReadLine());
+        N2 = N1 * N1;
+        Console.WriteLine("el area es:" + N2);
+    }
+}
---
+++ FabricioAduviri/practica3/ejercicio4/ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Bienvenidos a Programcion 1");
