ca5a4d4de20bff028aab95f36f377287b853a975
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 14:44:10 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Ejercicio2
+{
+    static void Main()
+    {
+        int N1, N2, Suma;
+        Console.Write("ingrese primer numero:");
+        N1 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("ingrese segundo numero:");
+        N2 = Convert.ToInt32(Console.ReadLine());
+
+        Suma = N1 + N2;
+
+        Console.WriteLine("el resultado es:" + Suma);
+    }
+}
