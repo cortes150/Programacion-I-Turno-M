e0108603823517b8a2db465e6554629f1f3cf8bb
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 14:47:43 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio7/ejercicio7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class Ejercicio7
+{
+    static void Main()
+    {
+        int N;
+        Console.Write("inserte un numero:");
+        N = Convert.ToInt32(Console.ReadLine());
+        if (N / 2 == 0) 
+        {
+            Console.WriteLine("Par");
+        }
+        else
+        {
+            Console.WriteLine("Impar");
+        }
+    }
+}
