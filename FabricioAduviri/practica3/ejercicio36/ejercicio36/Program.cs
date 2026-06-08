14a55d01ecbde830b29388cebdcfbf54917b2252
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 23:06:52 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio36/ejercicio36/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+
+        Console.Write("Ingrese un número: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+            Console.WriteLine("Positivo");
+        else if (num < 0)
+            Console.WriteLine("Negativo");
+        else
+            Console.WriteLine("Cero");
+    }
+}
