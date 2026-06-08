293b2fe07201e2220db38d5445d9dfc62318d51c
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 22:56:01 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio27/ejercicio27/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string? Gusto;
+        Console.Write("Elija su gusto: ");
+        Gusto=Console.ReadLine();
+        
+        if( Gusto == "matematicas") 
+        {
+            Console.WriteLine("INGENIERIA");
+        }
+        else 
+        {
+            if (Gusto == "Diseño")
+            {
+                Console.WriteLine("MULTIMEDIA");
+            }
+            else { Console.WriteLine("AUDITORIA"); }
+        }
+        Console.WriteLine("Ingresaste: " + Gusto);
+    }
+}
---
+++ FabricioAduviri/practica3/ejercicio28/ejercicio28/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int H;
+        Console.Write("Ingrese el tiempo: ");
+        H= Convert.ToInt32(Console.ReadLine());
+        if (H < 2) 
+        {
+            Console.WriteLine("Costo 10Bs");
+        }
+        else 
+        {
+            if (H <= 5)
+            {
+                Console.WriteLine("Costo 20Bs");
+            }
+            else { Console.WriteLine("Costo 35Bs"); }
+
+        }
+    }
+}
