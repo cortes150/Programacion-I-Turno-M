c37570879060f9a99ba910c2de2b95d6baacbaff
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 22:39:16 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio10/ejercicio10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N1;
+        Console.Write("Ingresa tu edad:");
+        N1 = Convert.ToInt32(Console.ReadLine());
+        if (N1 >= 18)
+        {
+            Console.WriteLine("Mayor de edad");
+        }
+        else
+        {
+            Console.WriteLine("Menor de edad");
+
+        }
+    }
+}
---
+++ FabricioAduviri/practica3/ejercicio11/ejercicio11/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Ejercicio11
+{
+    private static void Main(string[] args)
+    {
+        int N1, N2, N3, N4;
+        Console.Write("ingrese la cantidad de personas:");
+        N1 = Convert.ToInt32(Console.ReadLine());
+        N2 = N1 * 35;
+        if (N1 > 4)
+        {
+            N3 = N2 * 20 / 100;
+
+        }
+        else
+        {
+            N3 = 0;
+        }
+        N4 = N2 - N3;
+        Console.WriteLine(N4);
+
+    }
+}
