1ce98efde0f952a0583b5da98eb0aa1624fe0dfa
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 22:45:55 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio12/ejercicio12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N1, N2, N3;
+        Console.Write("cantidad:");
+        N1 = Convert.ToInt32(Console.ReadLine());
+        N2 = N1 * 15;
+        if (N1 >= 3)
+        {
+            N3 = N2 - 10;
+        }
+        else
+        {
+            N3 = N2;
+        }
+        Console.WriteLine("Pagar:" + N3);
+    }
+}
