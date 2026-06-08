4deee65b3177baf7ef706c8dfcd915da5a5513b6
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 22:51:01 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio23/ejercicio23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double P, I, T;
+        Console.Write("Agregue el precio del producto: ");
+        P=Convert.ToDouble(Console.ReadLine());
+        if (P > 1000)
+        {
+            I = P * 0.13;
+        }
+        else
+        {
+            I = 0;
+        }
+        T = P - I;
+        Console.WriteLine("el monto a pagar es: " + T);
+    }
+}
---
+++ FabricioAduviri/practica3/ejercicio24/ejercicio24/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int P;
+        Console.Write("Puntos: ");
+        P=Convert.ToInt32(Console.ReadLine());
+        if (P > 1000)
+        {
+            Console.WriteLine("3 vidas extras");
+        }
+        else
+        {
+            if (P >= 500)
+            {
+                Console.WriteLine("2 vidas extras");
+            }
+            else
+            {
+                Console.WriteLine("1 vida extra");
+            }
+        }
+    }
+}
