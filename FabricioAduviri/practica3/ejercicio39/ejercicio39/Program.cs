8063a1777f9c77389179a06d29273bac5f2dad12
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 23:10:00 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio39/ejercicio39/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double monto, total;
+        string pago;
+
+        Console.Write("Monto: ");
+        monto = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Método de pago: ");
+        pago = Console.ReadLine();
+
+        if (pago == "Efectivo")
+            total = monto - (monto * 0.15);
+        else if (pago == "Tarjeta")
+            total = monto - (monto * 0.05);
+        else
+            total = monto - (monto * 0.10);
+
+        Console.WriteLine("Total: " + total);
+    }
+}
---
+++ FabricioAduviri/practica3/ejercicio40/ejercicio40/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double estatura;
+        string finSemana;
+        double precio;
+
+        Console.Write("Estatura: ");
+        estatura = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("¿Fin de semana? (SI/NO): ");
+        finSemana = Console.ReadLine();
+
+        if (estatura < 1)
+            precio = 0;
+        else if (estatura <= 1.5)
+            precio = 20;
+        else
+            precio = 40;
+
+        if (finSemana == "SI")
+            precio += 10;
+
+        Console.WriteLine("Total: " + precio + " Bs");
+    }
+}
