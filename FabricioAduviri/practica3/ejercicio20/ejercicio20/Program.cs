b8eb19f389c23cd2bec30908ef12c25e36d614e6
fabricioaduviri <fabriadu@gmail.com>
2026-06-07 22:48:17 -0400

    Add files via upload

---
+++ FabricioAduviri/practica3/ejercicio20/ejercicio20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int E;
+        Console.Write("Edad: ");
+        E = Convert.ToInt32(Console.ReadLine());
+        if (E >= 18)
+        {
+            Console.WriteLine("Puede ingresar");
+        }
+        else 
+        {
+            Console.WriteLine("No puede ingresar");
+        }
+    }
+}
---
+++ FabricioAduviri/practica3/ejercicio21/ejercicio21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Ejercicio21
+{
+    static void Main()
+    {
+        double saldo, monto;
+        Console.Write("Ingrese saldo:");
+        saldo=Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese monto:");
+        monto=Convert.ToDouble(Console.ReadLine());
+        if (saldo <= monto)
+        {
+            Console.WriteLine("Saldo insuficiente");
+        }
+        else
+        {
+            Console.WriteLine("Operacion valida");
+        }
+    }
+}
