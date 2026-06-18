106aaed9204df55809ee11ae494261b7bc50be1e
chura4909-gif <chura4909@gmail.com>
2026-06-18 10:13:25 -0400

    Agregar tarea PDF

---
+++ Programacion_I_Maniana/Untitled-1.py
@@ -0,0 +1,9 @@
+for i in range(1, 11):
+    print(i) 
+    for j in rango (1, 11):
+        print(j)
+        print(i * j)
+        for k in range(1, 11):
+            print(k)
+            print(i * j * k)
+            
